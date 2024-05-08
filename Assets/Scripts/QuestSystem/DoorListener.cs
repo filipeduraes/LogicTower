f9d00b4586074cdff177004c015a53d0db3b97b0
Filipe Durães <77279496+filipeduraes@users.noreply.github.com>
2024-05-08 15:05:19 -0300

    Add door listener and setup urp

---
+++ Assets/Scripts/QuestSystem/DoorListener.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+
+namespace LogicTower.QuestSystem
+{
+    public class DoorListener : MonoBehaviour
+    {
+        [SerializeField] private Transform levelTrigger;
+        [SerializeField] private Animator doorAnimator;
+        [SerializeField] private string openState;
+        [SerializeField] private string closeState;
+
+        private void OnEnable()
+        {
+            QuestManager.OnQuestFinishedChanged += SetDoorIsOpenState;
+        }
+
+        private void OnDisable()
+        {
+            QuestManager.OnQuestFinishedChanged -= SetDoorIsOpenState;
+        }
+
+        private void SetDoorIsOpenState(bool isOpen)
+        {
+            doorAnimator.Play(isOpen ? openState : closeState);
+            levelTrigger.gameObject.SetActive(isOpen);
+        }
+    }
+}
